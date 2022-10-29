de982bc9c3dadfa25b9ac35f30c3e3c8d13954f3
Ethan kostiuk <ethan.kostiuk@gmail.com>
2022-10-29 16:11:43 -0600

    update

--- Character.py
+++ Character.py
@@ -8,15 +8,21 @@ class Character:
         self.parent_screen = parent_Screen
         
 class Player(Character):
+    playerX_change = 0
+    playerY_change = 0
+
     def __init__(self, location, x, y, parent_screen):
         super().__init__(location, x, y, parent_screen)
         pass
     
     def  moveUp(self):
-        self.y -= 0.2
+        self.playerY_change -= 0.2
     def  moveDown(self):
-        self.y += 0.2
+        self.playerY_change += 0.2
     def  moveLeft(self):
-        self.x -= 0.2
+        self.playerX_change -= 0.2
     def  moveRight(self):
-        self.x += 0.2
+        self.playerX_change += 0.2
+    def  updatePlayer(self):
+        self.x += self.playerX_change 
+        self.y += self.playerY_change
