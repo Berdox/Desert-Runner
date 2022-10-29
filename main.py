9220af041edd6933f0f387d6694a224fc4f17b0c
Ethan kostiuk <ethan.kostiuk@gmail.com>
2022-10-29 14:45:32 -0600

    Implemented Character X and Y movement

--- main.py
+++ main.py
@@ -2,11 +2,31 @@ import pygame
 import random
 import math
 
+# Initialize pygame
 pygame.init()  
-screen = pygame.display.set_mode((400,500))
-pygame.display.set_caption('uhhhh')  
-done = False  
 
+# Create Screen
+screen = pygame.display.set_mode((480,800))
+
+# Caption
+pygame.display.set_caption('uhhhh')
+
+# Icon
+icon = pygame.image.load('./resource/pumpkin.png')
+pygame.display.set_icon(icon)
+
+# Background
+
+# Sound
+
+# Player
+playerImg = pygame.image.load('./resource/block.jpg')
+playerX = 220
+playerY = 600
+playerX_change = 0
+playerY_change = 0
+
+"""
 # Enemy 
 enemyImg = []
 enemyX = []
@@ -19,16 +39,46 @@ for i in range(enemyNum):
 
 # Game Over
 game_over_font = pygame.font.Font('over.ttf', 32)
-
+"""
 def dspGameOver():
     game_over = game_over_font.render("GAME OVER", True,(255, 255, 255))
 
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
 # Game Loop
+done = False 
 while not done:  
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:  
             done = True  
 
+        # Check Keystroke
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.2
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.2
+            if event.key == pygame.K_UP:
+                playerY_change = -0.2
+            if event.key == pygame.K_DOWN:
+                playerY_change = 0.2
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                playerY_change = 0
+
+    playerX += playerX_change
+    playerY += playerY_change
+
+
+    screen.fill((0, 0, 0))
+
+    player(playerX, playerY)
+    pygame.display.update()
+"""
     # Enemy Movement
     for i in range(enemyNum):
         # Game Over 
@@ -39,3 +89,4 @@ while not done:
             break
 
     pygame.display.flip()  
+"""
