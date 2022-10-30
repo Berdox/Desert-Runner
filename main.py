82153fca25bfc72510ead5943991162c4e42abca
kose2888 <ethan.kostiuk@gmail.com>
2022-10-30 15:59:57 -0600

    Fixed zombie hitbox

--- main.py
+++ main.py
@@ -98,10 +98,10 @@ while running:
         score_value += 1
     back.run()
     utili.redraw(player)
-    pygame.mixer.Sound.play(rumble)
+    #pygame.mixer.Sound.play(rumble)
     
     if over != True:
-        spawn.spawnZombie(utili.resize(pygame.image.load("./resource/zombie.png").convert_alpha(), 0.3), screen)
+        spawn.spawnZombie(utili.resize(pygame.image.load("./resource/zombie1.png").convert_alpha(), 0.3), screen)
     else:
         show_Game_Over()
         show_Play_Again()
